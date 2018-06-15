40512549954e8d283f5e13729ad8b171c0607d96
dfreidin <36039896+dfreidin@users.noreply.github.com>
2018-06-15 02:54:00 -0700

    added resume and python video

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,8 @@
 jQuery(document).ready(function($) {
   "use strict";
 
+  $('#game_knight_video').collapse("hide");
+
   //add some elements with animate effect
   $('.icon').addClass("animated fadeInDown");
   $("#intro").hover(
